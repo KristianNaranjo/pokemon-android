e14cab4a83e2458d98154473c6ba78a6017d6c08
KristianNaranjo <kristiannaranjo@gmail.com>
2019-12-02 22:37:06 -0500

    Added all colors for types. Also alphabetized types

--- app/src/main/java/com/naranjo/kristian/pokemonandroid/datastore/Models.kt
+++ app/src/main/java/com/naranjo/kristian/pokemonandroid/datastore/Models.kt
@@ -13,77 +13,77 @@ data class Pokemon(
     @Json(name = "url") val imageUrl: String
 ) : Parcelable {
     enum class Type {
-        @Json(name = "water")
-        WATER {
-            override val colorResId: Int = 0
-        },
-        @Json(name = "fire")
-        FIRE {
-            override val colorResId: Int = R.color.fireRed
-        },
-        @Json(name = "grass")
-        GRASS {
-            override val colorResId: Int = 0
-        },
-        @Json(name = "ice")
-        ICE {
-            override val colorResId: Int = R.color.iceBlue
-        },
-        @Json(name = "fighting")
-        FIGHTING {
-            override val colorResId: Int = 0
-        },
-        @Json(name = "dragon")
-        DRAGON {
-            override val colorResId: Int = R.color.dragonBlue
-        },
-        @Json(name = "flying")
-        FLYING {
-            override val colorResId: Int = 0
-        },
-        @Json(name = "electric")
-        ELECTRIC {
-            override val colorResId: Int = 0
-        },
-        @Json(name = "normal")
-        NORMAL {
-            override val colorResId: Int = 0
-        },
-        @Json(name = "fairy")
-        FAIRY {
-            override val colorResId: Int = R.color.fairyPink
-        },
         @Json(name = "bug")
         BUG {
             override val colorResId: Int = R.color.bugGreen
-        },
-        @Json(name = "psychic")
-        PSYCHIC {
-            override val colorResId: Int = 0
-        },
-        @Json(name = "steel")
-        STEEL {
-            override val colorResId: Int = 0
         },
         @Json(name = "dark")
         DARK {
             override val colorResId: Int = R.color.darkBrown
         },
-        @Json(name = "rock")
-        ROCK {
-            override val colorResId: Int = 0
+        @Json(name = "dragon")
+        DRAGON {
+            override val colorResId: Int = R.color.dragonBlue
+        },
+        @Json(name = "electric")
+        ELECTRIC {
+            override val colorResId: Int = R.color.electricYellow
+        },
+        @Json(name = "fairy")
+        FAIRY {
+            override val colorResId: Int = R.color.fairyPink
+        },
+        @Json(name = "fighting")
+        FIGHTING {
+            override val colorResId: Int = R.color.fightingMaroon
+        },
+        @Json(name = "fire")
+        FIRE {
+            override val colorResId: Int = R.color.fireRed
+        },
+        @Json(name = "flying")
+        FLYING {
+            override val colorResId: Int = R.color.flyingPurple
+        },
+        @Json(name = "ghost")
+        GHOST {
+            override val colorResId: Int = R.color.ghostPurple
+        },
+        @Json(name = "grass")
+        GRASS {
+            override val colorResId: Int = R.color.grassGreen
         },
         @Json(name = "ground")
         GROUND {
-            override val colorResId: Int = 0
+            override val colorResId: Int = R.color.groundBeige
+        },
+        @Json(name = "ice")
+        ICE {
+            override val colorResId: Int = R.color.iceBlue
+        },
+        @Json(name = "normal")
+        NORMAL {
+            override val colorResId: Int = R.color.normalBrown
         },
         @Json(name = "poison")
         POISON {
             override val colorResId: Int = R.color.poisonPurple
         },
-        @Json(name = "ghost")
-        GHOST {
-            override val colorResId: Int = 0
+        @Json(name = "psychic")
+        PSYCHIC {
+            override val colorResId: Int = R.color.psychicPink
+        },
+        @Json(name = "rock")
+        ROCK {
+            override val colorResId: Int = R.color.rockBrown
+        },
+        @Json(name = "steel")
+        STEEL {
+            override val colorResId: Int = R.color.steelGrey
+        },
+        @Json(name = "water")
+        WATER {
+            override val colorResId: Int = R.color.waterBlue
         };
 
         abstract val colorResId: Int
