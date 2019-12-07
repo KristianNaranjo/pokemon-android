e8f5e0b6acf2da665a4b247560be00477c26c110
KristianNaranjo <kristiannaranjo@gmail.com>
2019-12-07 13:35:45 -0500

    Fixed places that are using observable for pokemon list now

--- app/src/main/java/com/naranjo/kristian/pokemonandroid/ui/details/PokemonDetailsActivity.kt
+++ app/src/main/java/com/naranjo/kristian/pokemonandroid/ui/details/PokemonDetailsActivity.kt
@@ -5,10 +5,13 @@ import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.jakewharton.rxbinding3.recyclerview.dataChanges
 import com.naranjo.kristian.pokemonandroid.R
+import com.naranjo.kristian.pokemonandroid.datastore.PokemonDataStore
 import com.naranjo.kristian.pokemonandroid.service.Pokemon
 import com.naranjo.kristian.pokemonandroid.service.PokemonJsonManager
 import com.naranjo.kristian.pokemonandroid.ui.base.BaseActivity
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.rxkotlin.plusAssign
+import io.reactivex.schedulers.Schedulers
 import org.koin.android.ext.android.inject
 
 class PokemonDetailsActivity : BaseActivity() {
@@ -16,7 +19,7 @@ class PokemonDetailsActivity : BaseActivity() {
         const val EXTRA_POKEMON = "extraPokemon"
     }
 
-    private val datastore: PokemonJsonManager by inject()
+    private val pokemonDataStore: PokemonDataStore by inject()
 
     private lateinit var pokemonDetails: RecyclerView
 
@@ -26,14 +29,26 @@ class PokemonDetailsActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
 
         val pokemon = intent.getParcelableExtra(EXTRA_POKEMON) as Pokemon
-        val pokemonList = datastore.pokemon
         val snapHelper = PagerSnapHelper()
         snapHelper.attachToRecyclerView(pokemonDetails)
         val pokemonDetailsAdapter = PokemonDetailsAdapter()
         pokemonDetails.adapter = pokemonDetailsAdapter
-        pokemonDetailsAdapter.submitList(pokemonList)
-        disposables += pokemonDetailsAdapter.dataChanges()
-                .subscribe { pokemonDetails.scrollToPosition(pokemonList.indexOf(pokemon)) }
+
+        val pokemonList = mutableListOf<Pokemon>()
+        disposables += pokemonDataStore.pokemon
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .flatMap {
+                    pokemonDetailsAdapter.submitList(it)
+                    pokemonList.addAll(it)
+                    pokemonDetailsAdapter.dataChanges()
+                }
+                .subscribe {
+                    val position = pokemonList.indexOf(pokemon)
+                    if (position != -1) {
+                        pokemonDetails.scrollToPosition(pokemonList.indexOf(pokemon))
+                    }
+                }
     }
 
     override fun bindViews() {
