8df438634bb082212f3a19286398835b4b91230b
KristianNaranjo <kristiannaranjo@gmail.com>
2020-04-02 22:33:32 -0400

    Added PageTransformer to let you see multiple items on screen at once

--- app/src/main/java/com/naranjo/kristian/pokemonandroid/ui/details/PokemonDetailsActivity.kt
+++ app/src/main/java/com/naranjo/kristian/pokemonandroid/ui/details/PokemonDetailsActivity.kt
@@ -2,7 +2,9 @@ package com.naranjo.kristian.pokemonandroid.ui.details
 
 import android.os.Bundle
 import android.widget.TextView
+import androidx.core.view.ViewCompat
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.flexbox.FlexDirection
 import com.google.android.flexbox.FlexWrap
@@ -17,6 +19,7 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.rxkotlin.plusAssign
 import io.reactivex.schedulers.Schedulers
 import org.koin.android.ext.android.inject
+import kotlin.math.abs
 
 class PokemonDetailsActivity : BaseActivity() {
     companion object {
@@ -98,7 +101,26 @@ class PokemonDetailsActivity : BaseActivity() {
 
     override fun bindViews() {
         name = findViewById(R.id.details_name)
-        pokemonImages = findViewById(R.id.images)
+        pokemonImages = findViewById<ViewPager2>(R.id.images).apply {
+            offscreenPageLimit = 3
+            setPageTransformer(CompositePageTransformer().apply {
+                val pageMarginPx = resources.getDimensionPixelOffset(R.dimen.details_image_page_margin)
+                val offsetPx = resources.getDimensionPixelOffset(R.dimen.details_image_offset)
+                addTransformer { page, position ->
+                    val viewPager = page.parent.parent as ViewPager2
+                    val offset = position * -(2 * offsetPx + pageMarginPx)
+                    if (viewPager.orientation == ViewPager2.ORIENTATION_HORIZONTAL) {
+                        if (ViewCompat.getLayoutDirection(viewPager) == ViewCompat.LAYOUT_DIRECTION_RTL) {
+                            page.translationX = -offset
+                        } else {
+                            page.translationX = offset
+                        }
+                    } else {
+                        page.translationY = offset
+                    }
+                }
+            })
+        }
         number = findViewById(R.id.details_number)
         types = findViewById(R.id.details_types)
         weaknesses = findViewById(R.id.details_weaknesses)
